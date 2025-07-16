35d0d7ba3f6ac529462a3fef1d05e609fcface70
Ed Bray <edward.bray@uts.edu.au>
2025-07-17 08:15:06 +1000

    Remove extraneous turltlebot4 launch files, and just spawn the chosen gazebo world

--- launch/41068_ignition.launch.py
+++ launch/41068_ignition.launch.py
@@ -1,76 +1,37 @@
-# Copyright 2023 Clearpath Robotics, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# @author Roni Kreinin (rkreinin@clearpathrobotics.com)
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
-
-ARGUMENTS = [
-    DeclareLaunchArgument('namespace', default_value='',
-                          description='Robot namespace'),
-    DeclareLaunchArgument('rviz', default_value='false',
-                          choices=['true', 'false'], description='Start rviz.'),
-    DeclareLaunchArgument('world', default_value='simple_trees',
-                          description='Ignition World'),
-    DeclareLaunchArgument('model', default_value='standard',
-                          choices=['standard', 'lite'],
-                          description='Turtlebot4 Model'),
-]
-
-for pose_element in ['x', 'y', 'z', 'yaw']:
-    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
-                     description=f'{pose_element} component of the robot pose.'))
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Directories
-    pkg_41068_ignition_bringup = get_package_share_directory(
-        '41068_ignition_bringup')
 
-    # Paths
-    ignition_launch = PathJoinSubstitution(
-        [pkg_41068_ignition_bringup, 'launch', 'ignition.launch.py'])
-    robot_spawn_launch = PathJoinSubstitution(
-        [pkg_41068_ignition_bringup, 'launch', 'turtlebot4_spawn.launch.py'])
+    ld = LaunchDescription()
 
-    ignition = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([ignition_launch]),
-        launch_arguments=[
-            ('world', LaunchConfiguration('world'))
-        ]
+    # Get paths to directories
+    pkg_path = FindPackageShare('41068_ignition_bringup')
+    config_path = PathJoinSubstitution([pkg_path,
+                                       'config'])
+    worlds_path = PathJoinSubstitution([pkg_path,
+                                       'worlds'])
+
+    # Start Gazebo to simulate the robot in the chosen world
+    world_launch_arg = DeclareLaunchArgument(
+        'world',
+        default_value='simple_trees',
+        description='Which world to load',
+        choices=['simple_trees', 'large_demo']
     )
-
-    robot_spawn = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([robot_spawn_launch]),
-        launch_arguments=[
-            ('namespace', LaunchConfiguration('namespace')),
-            ('rviz', LaunchConfiguration('rviz')),
-            ('x', LaunchConfiguration('x')),
-            ('y', LaunchConfiguration('y')),
-            ('z', LaunchConfiguration('z')),
-            ('yaw', LaunchConfiguration('yaw'))]
+    ld.add_action(world_launch_arg)
+    gazebo = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('ros_ign_gazebo'),
+                             'launch', 'ign_gazebo.launch.py']),
+        launch_arguments={
+            'ign_args': [PathJoinSubstitution([worlds_path,
+                                               [LaunchConfiguration('world'), '.sdf']]),
+                         ' -r']}.items()
     )
+    ld.add_action(gazebo)
 
-    # Create launch description and add actions
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(ignition)
-    ld.add_action(robot_spawn)
     return ld
