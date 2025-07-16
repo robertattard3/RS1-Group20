f451c0287aaa41abbd1a0c2601de33dd354101ea
Ed Bray <edward.bray@uts.edu.au>
2025-07-17 09:37:14 +1000

    Copy mars rover URDF from space robotics

--- launch/41068_ignition.launch.py
+++ launch/41068_ignition.launch.py
@@ -1,7 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (Command, LaunchConfiguration,
+                                  PathJoinSubstitution)
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -13,8 +15,30 @@ def generate_launch_description():
     pkg_path = FindPackageShare('41068_ignition_bringup')
     config_path = PathJoinSubstitution([pkg_path,
                                        'config'])
-    worlds_path = PathJoinSubstitution([pkg_path,
-                                       'worlds'])
+
+    # Additional command line arguments
+    use_sim_time_launch_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='True',
+        description='Flag to enable use_sim_time'
+    )
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    ld.add_action(use_sim_time_launch_arg)
+
+    # Load robot_description and start robot_state_publisher
+    robot_description_content = ParameterValue(
+        Command(['xacro ',
+                 PathJoinSubstitution([pkg_path,
+                                       'urdf',
+                                       'mars_explorer.urdf.xacro'])]),
+        value_type=str)
+    robot_state_publisher_node = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[{
+                                          'robot_description': robot_description_content,
+                                          'use_sim_time': use_sim_time
+                                      }])
+    ld.add_action(robot_state_publisher_node)
 
     # Start Gazebo to simulate the robot in the chosen world
     world_launch_arg = DeclareLaunchArgument(
@@ -28,10 +52,20 @@ def generate_launch_description():
         PathJoinSubstitution([FindPackageShare('ros_ign_gazebo'),
                              'launch', 'ign_gazebo.launch.py']),
         launch_arguments={
-            'ign_args': [PathJoinSubstitution([worlds_path,
+            'ign_args': [PathJoinSubstitution([pkg_path,
+                                               'worlds',
                                                [LaunchConfiguration('world'), '.sdf']]),
                          ' -r']}.items()
     )
     ld.add_action(gazebo)
+
+    # Spawn robot in Gazebo
+    robot_spawner = Node(
+        package='ros_ign_gazebo',
+        executable='create',
+        output='screen',
+        arguments=['-topic', '/robot_description']
+    )
+    ld.add_action(robot_spawner)
 
     return ld
